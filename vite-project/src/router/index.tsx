8eaa08b0c8bdc1309a27e1fb39fa0be7a88b7856
huangyiran <huangyiran@dreame.tech>
2024-08-06 15:17:32 +0800

    feat: 菜单功能

--- vite-project/src/router/index.tsx
+++ vite-project/src/router/index.tsx
@@ -1,40 +1,26 @@
-import { createHashRouter, Navigate } from 'react-router-dom'
-import Login from '@/pages/Login'
-import Home from '@/pages/Home'
+import { createBrowserRouter } from 'react-router-dom'
 import MainPage from '@/pages/MainPage'
-import Account from '@/pages/Account'
-
+import HomePage from '@/pages/HomePage'
 // 全局路由
-export const globalRouters = createHashRouter([
-    // 对精确匹配"/login"，跳转Login页面
-    {
-        path: '/login',
-        element: <Login />,
-    },
-    // 精确匹配"/home"，跳转Home页面
-    {
-        path: '/home',
-        element: <Home />,
-    },
-    // 精确匹配"/account"，跳转Account页面
-    {
-        path: '/account',
-        element: <Account />,
-    },
-    // 如果URL没有"#路由"，跳转Home页面
+export const globalRouters = createBrowserRouter([
     {
         path: '/',
-        element: <Home />,
+        element: <HomePage />,
+        children: [
+            {
+                path: 'main',
+                element: <MainPage />,
+            },
+            {
+                path: 'personal',
+                element: <HomePage />,
+            },
+            {
+                path: 'setting',
+                element: <MainPage />,
+            },
+        ]
     },
-     // 如果URL没有"#路由"，跳转Home页面
-     {
-        path: '/main',
-        element: <MainPage />,
-    },
-    // 未匹配，，跳转Login页面
-    {
-        path: '*',
-        element: <Navigate to="/login" />,
-    },
+
 ])
 
